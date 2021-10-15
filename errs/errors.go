1dcc6df61f4481df4d2b20db150833c4f0d44fb0
d.lozhkin <d.lozhkin@geekbrains.ru>
2021-10-15 10:18:01 +0300

    init

---
+++ errs/errors.go
@@ -0,0 +1,42 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
+}
+
+func NewDatabaseError() *AppError {
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: "unexpected database error",
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnprocessableEntity,
+		Message: message,
+	}
+}
+
+func NewUnauthorizedError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
